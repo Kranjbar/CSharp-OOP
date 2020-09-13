37731dbfc7e41b4578712be2c829a45529ff7253
Kayvon Ranjbar <kayvon.ranjbar@yahoo.com>
2020-09-13 13:09:25 -0600

    Finished separating responsibilities

--- ACM/ACM.BL/OrderRepository.cs
+++ ACM/ACM.BL/OrderRepository.cs
@@ -21,9 +21,10 @@ namespace ACM.BL
 
             // Temporary hard-coded values to return
             // a populated order
-            if (orderId == 1)
+            if (orderId == 10)
             {
-                order.OrderDate = DateTimeOffset.Now;
+                order.OrderDate = new DateTimeOffset(DateTime.Now.Year, 4, 14, 10, 00, 00,
+                                                     new TimeSpan(7, 0, 0));
             }
 
             return order;
