3604f95dfb322cbb120ceb397444ef9426cfa130
Kayvon Ranjbar <kayvon.ranjbar@yahoo.com>
2020-09-19 10:53:53 -0600

    Built reusable components

--- ACM/ACM.BL/Product.cs
+++ ACM/ACM.BL/Product.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Acme.Common;
 
 namespace ACM.BL
 {
@@ -19,9 +20,22 @@ namespace ACM.BL
         }
 
         public int ProductId { get; private set; }
-        public string ProductName { get; set; }
         public string ProductDescription { get; set; }
         public decimal? CurrentPrice { get; set; }
+
+        private string _productName;
+
+        public string ProductName
+        {
+            get
+            {
+                return _productName.InsertSpaces();
+            }
+            set
+            {
+                _productName = value;
+            }
+        }
 
         public override string ToString() => ProductName;
 
---
+++ ACM/Tests/Acme.CommonTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Acme.CommonTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Acme.CommonTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8e3b1681-c8de-4abf-a35d-c5c4b9375622")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
